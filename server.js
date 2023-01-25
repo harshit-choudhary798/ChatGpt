9ca698d0a594a77928fb51cabba963117e0f97b1
Harshit Choudhary <harshit1840077@gmail.com>
2023-01-25 23:09:47 +0530

    final commit

--- server.js
+++ server.js
@@ -7,12 +7,49 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 const bodyparser = require('body-parser')
 
+require("dotenv").config();
+
+
 
 var PORT = process.env.port || 3000
 // Body-parser middleware
 
+
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
+
+
+
+const configuration = new Configuration({
+    organization: process.env.ORG,
+    apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+app.get("/", function(req, res){
+	res.render("index")
+});
+
+app.post('/saveData',async (req, res) => {
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: 
+
+		`${req.body.abcd}?`,
+		max_tokens: 1000,
+		temperature: 0,
+	  });
+	console.log("response: ",response)
+	if(response.data.choices[0].text){
+		res.render("text",{
+			items:response.data.choices[0].text
+		});
+
+	}
+	
+	console.log("Using Body-parser: ", req.body.abcd)
+})
 
 app.listen(PORT, function(error){
 	if (error) throw error
